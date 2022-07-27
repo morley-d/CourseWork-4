bfb6729e71b31aadac720a948f0f39888f110e3d
San <andreal87@mail.ru>
2022-07-27 10:17:58 +0600

    create packege data_scripts

--- run.py
+++ run.py
@@ -1,33 +1,22 @@
 import os
 os.environ['FLASK_ENV'] = 'development'
-from flask import Flask, render_template
-from flask_restx import Api
-from project.config import DevelopmentConfig
+# from flask import Flask, render_template
+# from flask_restx import Api
+# from project.config import DevelopmentConfig
 from project.config import config
 from project.models import Genre
 from project.server import create_app, db
-
-# api = Api(title="Flask Course Project 4", doc="/docs")
-
-    # @app.route('/')
-    # def index():
-    #     return render_template('index.html')
-    #
-    # db.init_app(app)
-    # api.init_app(app)
-    #
-    # return app
 
 
 app = create_app(config)
 
 
-# @app.shell_context_processor
-# def shell():
-#     return {
-#         "db": db,
-#         "Genre": Genre,
-#     }
+@app.shell_context_processor
+def shell():
+    return {
+        "db": db,
+        "Genre": Genre,
+    }
 
 if __name__ == '__main__':
     app.run(port=7777, debug=True)
